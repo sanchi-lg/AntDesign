9f909fb0903869c7b336ac6a96e85d6d66e49f3d
sanchi-lg <75089221+sanchi-lg@users.noreply.github.com>
2021-03-18 22:26:33 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from "react-loader-spinner"
 import {
     HeartOutlined,
     HeartFilled,
@@ -17,7 +19,7 @@ export class Home extends Component {
         super(props)
 
         this.state = {
-            data: [], name: "", email: "", phone: "", website: "", femail: "", modalvisible: false
+            data: [], name: "", email: "", phone: "", website: "", femail: "", modalvisible: false, status: false
         }
 
     }
@@ -31,7 +33,9 @@ export class Home extends Component {
 
         axios.get("https://jsonplaceholder.typicode.com/users")
             .then(res => {
-                this.setState({ data: res.data })
+                this.setState({ data: res.data }, () => {
+                    this.setState({ status: true })
+                })
             })
             .catch(err => {
                 console.log(err);
@@ -90,126 +94,135 @@ export class Home extends Component {
         }
         return (
             <>
+                {/* <div> <DatePicker /></div> */}
+                {this.state.status ?
+                    <>
 
-                <Modal maskStyle={{ background: "transparent" }} title="Basic Modal" visible={this.state.modalvisible} footer={
-                    [
-                        <Button >Cancel</Button>,
-                        <Button type="primary" htmlType="submit" form="mform" >Ok</Button>
+                        <Modal maskStyle={{ background: "transparent" }} title="Basic Modal" visible={this.state.modalvisible} footer={
+                            [
+                                <Button >Cancel</Button>,
+                                <Button type="primary" htmlType="submit" form="mform" >Ok</Button>
 
-                    ]
-                }>
-                    <Form id="mform" size="medium" initialValues={{
-                        ["name"]: this.state.name, ["email"]: this.state.email, ["phone"]: this.state.phone, ["website"]: this.state.website
-                    }} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} onValuesChange={this.handle} onFinish={() => this.editok(this.state.femail)} >
+                            ]
+                        }>
+                            <Form id="mform" size="medium" initialValues={{
+                                ["name"]: this.state.name, ["email"]: this.state.email, ["phone"]: this.state.phone, ["website"]: this.state.website
+                            }} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} onValuesChange={this.handle} onFinish={() => this.editok(this.state.femail)} >
 
-                        <Form.Item
-                            name="name"
-                            label="Name"
+                                <Form.Item
+                                    name="name"
+                                    label="Name"
 
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "This field is required"
-                                },
-                            ]}
-                        >
-                            <Input style={{ borderRadius: "4px" }} />
-                        </Form.Item>
-                        <Form.Item
-                            name="email"
-                            label="Email"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "This field is required"
+                                        },
+                                    ]}
+                                >
+                                    <Input style={{ borderRadius: "4px" }} />
+                                </Form.Item>
+                                <Form.Item
+                                    name="email"
+                                    label="Email"
 
-                            rules={[
-                                {
-                                    type: 'email',
-                                    message: 'Invalid email',
-                                },
-                                {
-                                    required: true,
-                                    message: "This field is required"
-
-
-                                },
-                            ]}
-                        >
-                            <Input style={{ borderRadius: "4px" }} />
-                        </Form.Item>
-                        <Form.Item
-                            name="phone"
-                            label="Phone"
-
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "This field is required"
-
-                                },
-                            ]}
-                        >
-                            <Input style={{ borderRadius: "4px" }} />
-                        </Form.Item>
-                        <Form.Item
-                            name="website"
-                            label="Website"
-
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "This field is required"
-
-                                },
-                            ]}
-                        >
-                            <Input style={{ borderRadius: "4px" }} />
-                        </Form.Item>
-                    </Form>
-                </Modal>
-                <Row>
-
-                    {this.state.data.map(user =>
-                        <Col xs={24} sm={24} md={8} lg={8} xl={6}>
-                          
-                          <Card style={{ margin: "15px" }} cover={
-                                <div className="imgouter">
-                                    <img alt="img" src={`https://avatars.dicebear.com/v2/avataaars/${user.name}.svg?options[mood][]=happy`} height="200" width="200" />
-                                </div>
-                            }
-                                actions={
-                                    [
-                                        <Button className="op" type="text" user={user.email} onClick={() => { this.wish(user.email) }} icon={fun(user.email) ? <HeartFilled className="bodydeticon" style={{ color: "red" }} /> :
-                                            <HeartOutlined className="bodydeticon" style={{ color: "red" }} />}
-                                        />
-
-                                        ,
-                                        <Button icon={<EditOutlined className="bodydeticon" />} type="text" onClick={() => {
-                                            this.setState({ name: user.name, email: user.email, femail: user.email, phone: user.phone, website: user.website, modalvisible: true }, () => {
-                                            })
-                                        }} />
-
-                                        ,
-                                        <Button type="text" onClick={() => this.delete(user.email)} icon={<DeleteFilled className="bodydeticon" />} />
-                                    ]
-                                }>
-
-                                <h3>{user.name}</h3>
-                                <div className="bodydet">
-                                    <MailOutlined className="bodydeticon" />
-                                    <p style={{ marginLeft: "10px" }}>{user.email}</p>
-                                </div>
-                                <div className="bodydet">
-                                    <PhoneOutlined className="bodydeticon" />
-                                    <p style={{ marginLeft: "10px" }}>{user.phone}</p>
-                                </div>
-                                <div className="bodydet">
-                                    <GlobalOutlined className="bodydeticon" />
-                                    <p style={{ marginLeft: "10px" }}>{user.website}</p>
-                                </div>
+                                    rules={[
+                                        {
+                                            type: 'email',
+                                            message: 'Invalid email',
+                                        },
+                                        {
+                                            required: true,
+                                            message: "This field is required"
 
 
-                            </Card>
-                        </Col>
-                    )}
-                </Row>
+                                        },
+                                    ]}
+                                >
+                                    <Input style={{ borderRadius: "4px" }} />
+                                </Form.Item>
+                                <Form.Item
+                                    name="phone"
+                                    label="Phone"
+
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "This field is required"
+
+                                        },
+                                    ]}
+                                >
+                                    <Input style={{ borderRadius: "4px" }} />
+                                </Form.Item>
+                                <Form.Item
+                                    name="website"
+                                    label="Website"
+
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: "This field is required"
+
+                                        },
+                                    ]}
+                                >
+                                    <Input style={{ borderRadius: "4px" }} />
+                                </Form.Item>
+                            </Form>
+                        </Modal>
+                        <Row>
+
+                            {this.state.data.map(user =>
+                                <Col xs={24} sm={24} md={8} lg={8} xl={6}>
+                                  
+
+                                    <Card style={{ margin: "15px" }} cover={
+                                        <div className="imgouter">
+                                            <img alt="img" src={`https://avatars.dicebear.com/v2/avataaars/${user.name}.svg?options[mood][]=happy`} height="200" width="200" />
+                                        </div>
+                                    }
+                                        actions={
+                                            [
+                                                <Button className="op" type="text" user={user.email} onClick={() => { this.wish(user.email) }} icon={fun(user.email) ? <HeartFilled className="bodydeticon" style={{ color: "red" }} /> :
+                                                    <HeartOutlined className="bodydeticon" style={{ color: "red" }} />}
+                                                />
+
+                                                ,
+                                                <Button icon={<EditOutlined className="bodydeticon" />} type="text" onClick={() => {
+                                                    this.setState({ name: user.name, email: user.email, femail: user.email, phone: user.phone, website: user.website, modalvisible: true }, () => {
+                                                        console.log(this.state);
+                                                    })
+                                                }} />
+
+                                                ,
+                                                <Button type="text" onClick={() => this.delete(user.email)} icon={<DeleteFilled className="bodydeticon" />} />
+                                            ]
+                                        }>
+
+                                        <h3>{user.name}</h3>
+                                        <div className="bodydet">
+                                            <MailOutlined className="bodydeticon" />
+                                            <p style={{ marginLeft: "10px" }}>{user.email}</p>
+                                        </div>
+                                        <div className="bodydet">
+                                            <PhoneOutlined className="bodydeticon" />
+                                            <p style={{ marginLeft: "10px" }}>{user.phone}</p>
+                                        </div>
+                                        <div className="bodydet">
+                                            <GlobalOutlined className="bodydeticon" />
+                                            <p style={{ marginLeft: "10px" }}>{user.website}</p>
+                                        </div>
+
+
+                                    </Card>
+                                </Col>
+                            )}
+                        </Row>
+                    </>
+                    :
+                    <Loader type="Puff" color="blue" style={{ margin: "auto", fontWeight: "2px", paddingTop: "30%", width: "90vw", height: "90vh", textAlign: "center" }} />
+                }
             </>
         )
     }
